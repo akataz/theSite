55a4b9ae4cbb542216f4a398936631ecdcac6f1a
akataz <tyrus.brenc@gmail.com>
2016-12-01 04:31:39 -0800

    continue modifying file structure and pages to suit new site

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
-import React from 'react';
+ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
-import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
+ // eslint-disable-line import/no-named-as-default
+import blog from './containers/blog';
 import AboutPage from './components/AboutPage.js';
 import NotFoundPage from './components/NotFoundPage.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
-    <Route path="fuel-savings" component={FuelSavingsPage}/>
+    <Route path="blog" component={blog}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
