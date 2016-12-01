66ec0d5a556f317802668efd74b08bbf504f93d0
akataz <tyrus.brenc@gmail.com>
2016-12-01 03:53:33 -0800

    initial commit on personal website

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div>
         <IndexLink to="/">Home</IndexLink>
         {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
+        <Link to="/fuel-savings">Blog</Link>
         {' | '}
         <Link to="/about">About</Link>
         <br/>
