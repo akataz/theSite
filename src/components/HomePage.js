78b54f995b37382d596a18fc99cf12aeb0d6dd52
akataz <tyrus.brenc@gmail.com>
2016-12-02 06:35:54 -0800

    add first project showcase

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,11 +6,13 @@ const HomePage = () => {
     <div>
       <h1 className="heading">Tyrus Brenc</h1>
       <img src="./../assets/tyrus_headshot.jpg" height="40%" width="40%" id="profile"/>
-      <h2>Hello! Welcome to my personal website!  Please explore the links above for more.</h2>
-      <div>I am a Full Stack Software Engineer specializing in Javascript with a passion for diving deep into tech to figure out
-      which technologies work the best.  This website attempts to not only showcase
-      my work, but act as a proving ground for new tech that Im trying out!</div>
-      <h3>Recent Blog Post</h3>
+      <h2>Hello! Welcome to my website!  Please explore the links above for more.</h2>
+      <h4 className="textOnPage"> My name is Tyrus Brenc and I am a Full Stack Software Engineer specializing in Javascript with a passion for diving deep into tech to figure out
+      which technologies work together the best.  This website attempts to not only showcase
+      my work, but act as a proving ground for new tech that I&rsquo;m trying out!</h4>
+      <h2 className="headingSmall">Recent Projects</h2>
+
+      <img src="./../assets/apexSwipe.jpeg" />
     </div>
   );
 };
