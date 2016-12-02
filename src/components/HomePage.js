e7d1b1dc865c6b9d989efe6413b1ed257ee7a094
akataz <tyrus.brenc@gmail.com>
2016-12-02 03:17:59 -0800

    add profile pic to site

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div>
       <h1>Tyrus Brenc</h1>
       <Navbar/>
-
+      <img src="./../assets/tyrus_headshot.jpg" height="50%" width="50%" id="profile"/>
       <h2>Hello! Welcome to my personal website!  Please explore the links above for more.</h2>
       <div>I am a Full Stack Software Engineer specializing in Javascript with a passion for diving deep into tech to figure out
       which technologies work the best.  This website attempts to not only showcase
